4df12f90f3c903fe6ac41e1e35eb0df08da56621
Chen Xianmin <xianmin12@gmail.com>
2017-05-23 16:15:14 +0800

    change

---
+++ emacs/xm-spacemacs/elpa/tern-20161222.850/tern-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "tern" "20161222.850" "Tern-powered JavaScript integration" '((json "1.2") (cl-lib "0.5") (emacs "24")) :commit "a1cf72d9e2ed32ac3c75583cd9db75c5f981382d" :url "http://ternjs.net/")
